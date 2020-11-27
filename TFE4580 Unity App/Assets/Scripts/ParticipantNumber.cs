2218c7649328e909d11e97cc71c0006201828371
Peter Remøy Paulsen <30476405+petrepa@users.noreply.github.com>
2020-11-27 16:41:00 +0100

    Random participant number. Issue #23

---
+++ TFE4580 Unity App/Assets/Scripts/ParticipantNumber.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParticipantNumber : MonoBehaviour
+{
+    const string glyphs= "ABCDEFGHIJKLMNOP0123456789"; //add the characters you want
+    private Text text;
+
+    void Start(){
+        text = GetComponent<Text>();
+        var partNum = "";
+        
+        for(int i=0; i<4; i++)
+        {
+            partNum += (glyphs[Random.Range(0, glyphs.Length)]).ToString();
+        }
+
+        text.text = "#" + partNum;
+    }
+}
